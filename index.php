de87657b2fda0e542825d33a3f1acea562e3eaf8
purk <kimdonghyun0276@gmail.com>
2018-10-07 01:49:34 +0900

    - 로그인 후의 페이지(logged.html)의 디자인 일부 수정. - 위의 사항에 따라 base.css 역시 일부 수정됨. - index.php에서 설문 페이지 방문자의 수를 알기 위해 IP를 기록하는 소스 삽입. - IP 기록 소스는 PHP를 기반을 두고있기 때문에, 기존의 html파일은 삭제.

---
+++ index.php
@@ -0,0 +1,77 @@
+﻿<?php
+
+	/* 정식 출시에서는 미사용 */
+
+	function fileCheck($url) {
+
+		if(!file_exists($url))
+
+			mkdir($url, 0755, true);
+
+		return false;
+
+	}
+
+
+
+	function saveFile($url, $data) {
+
+		$ch=fopen($url, "w");
+
+		fwrite($ch, $data);
+
+		fclose($ch);
+
+		return false;
+
+	}
+
+	
+
+	$ip = $_SERVER["REMOTE_ADDR"];
+
+	$time = date("Ymd");
+
+
+
+	fileCheck("./day_ip_rec");
+
+	fileCheck("./day_ip_rec/".$time);
+
+    fileCheck("./day_ip_rec/".$time."/all");
+
+
+
+	saveFile("./day_ip_rec/".$time."/all/".$ip, time());
+
+    saveFile("./day_ip_rec/".$time."/".date("h")."/".$ip, time());
+
+	
+
+?>
+
+<script type="text/javascript">
+
+
+
+	window.onload=function() {
+
+
+
+		const url="https://goo.gl/forms/Vb1OXnxm2sZ2H2Y52";
+
+
+
+		alert("글귀 저장소의 설문 페이지로 이동합니다.");
+
+
+
+		location.href=url;
+
+
+
+	}
+
+
+
+</script>
